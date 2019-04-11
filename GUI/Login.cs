dafefb8ef48e3d99955f1ac97fc70279174ae809
CJBuchel <connorbuchel@gmail.com>
2019-04-11 11:52:09 +0800

    Added More Streamline User Interface

---
+++ GUI/Login.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace GUI
+{
+    public partial class Login : Form
+    {
+        MySqlConnection con = new MySqlConnection("server=localhost;user id=root;database=skynet;port=3308;persistsecurityinfo=True");
+        MySqlCommand cmd;
+        MySqlDataAdapter adpt;
+        DataTable dt;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuThinButton22_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin")
+            {
+                if (textBox1.Text == "admin")
+                {
+                    this.Hide();
+                    Main ss = new Main();
+                    ss.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Password Is Incorrect");
+                }
+            }
+            else if (textBox1.Text != "admin")
+            {
+                MySqlConnection con = new MySqlConnection("server=curtinfrc.duckdns.org;port=3306;user id=entity101;Password=entity101;database=entity101");
+                con.Open();
+                //MySqlCommand cmd = new MySqlCommand("select * from users where Username='" + textBox1.Text + "' and Password='" + textBox2.Text + "'", con);
+                MySqlDataAdapter sda = new MySqlDataAdapter("Select Count(*) From users where Username='" + textBox1.Text + "' and Password='" + textBox2.Text + "'", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows[0][0].ToString() == "1")
+                {
+                    this.Hide();
+                    Main ss = new Main();
+                    ss.Show();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Username and/or Password Is Incorrect");
+            }
+            
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
