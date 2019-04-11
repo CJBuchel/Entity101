4efb73db37cddeef971173c0f1943a9d62efa79c
CJBuchel <connorbuchel@gmail.com>
2019-04-11 09:47:18 +0800

    Updated Vision To a More Simplified version, with no threading

--- vision/src/main/cpp/main.cpp
+++ vision/src/main/cpp/main.cpp
@@ -1,8 +1,4 @@
-#include "Capture.h"
-#include "TapeProcessing.h"
-#include "ProcessController.h"
-#include "HatchProcessing.h"
-#include "Display.h"
+#include "vision.h"
 #include <iostream>
 #include <networktables/NetworkTableInstance.h>
 
@@ -12,15 +8,6 @@ int main(int argc, char **argv) {
   if (argc > 1) {
     team = std::stoi(argv[1]);
   }
-
-
-#ifdef __DESKTOP__
-  std::cout << "Running on Desktop - imshow enabled" << std::endl;
-  bool isDesktop = true;
-#else
-  std::cout << "Running embedded - imshow disabled" << std::endl;
-  bool isDesktop = false;
-#endif
 
   auto ntinst = nt::NetworkTableInstance::GetDefault();
   if (team != 0) {
@@ -32,40 +19,10 @@ int main(int argc, char **argv) {
     ntinst.StartServer();
   }
 
-  //Capture sideCapture{"HatchSide", isDesktop ? 1 : 5};
-  Capture frontCapture{"FrontSide", isDesktop ? 0 : 4};
-  
-  // TapeProcessing tapeSide{sideCapture};
-  // HatchProcessing hatchSide{sideCapture};
-  TapeProcessing tapeFront{frontCapture};
-  HatchProcessing hatchFront{frontCapture};
+  curtin_frc_vision vision;  
+  vision.run();
 
-  auto inst = nt::NetworkTableInstance::GetDefault();
-  auto visionTable = inst.GetTable("VisionTracking");
-
-  // Processing sideProcess{sideCapture, tapeSide, hatchSide, visionTable->GetEntry("Camera Set Side")};
-  Processing frontProcess{frontCapture, tapeFront, hatchFront, visionTable->GetEntry("Camera Set Front")};
-
-  // Display sideDisplay{"Side Display", sideProcess};
-  Display frontDisplay{"Front Display", frontProcess};
-
-  // sideCapture.StartThread(30.0);
-  // sideProcess.StartThread(30.0);
-  // sideDisplay.StartThread(30.0);
-
-  frontCapture.StartThread(30.0);
-  frontProcess.StartThread(30.0);
-  frontDisplay.StartThread(30.0);
-
-  // sideCapture.JoinThread();
-  // sideProcess.JoinThread();
-  // sideDisplay.JoinThread();
-
-  frontCapture.JoinThread();
-  frontProcess.JoinThread();
-  frontDisplay.JoinThread();
-
-  std::cout << "Vision Program Exited. Broken??" << std::endl;
+  std::cout << "Vision Program Exited. Was that meant to happen?" << std::endl;
   return -1;
 }
 #endif
